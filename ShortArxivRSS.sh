b0ed8a4654a0253fb88a14939e15cc2037efb465
dapingQ <zhenghao.yin@univie.ac.at>
2024-12-30 16:03:37 +0000

    Update Mon Dec 30 16:03:37 UTC 2024

--- ShortArxivRSS.sh
+++ ShortArxivRSS.sh
@@ -1,13 +1,13 @@
-# repository path
-REPO="$HOME/short_arxiv_rss"
+# repo_PATHsitory path
+REPO_PATH="$HOME/short_arxiv_rss"
 
 
-BASE_URL="https://rss.arxiv.org/rss"
-DOWNLOAD_FILE="${REPO}/raw_arxiv.rss"
-OUTPUT_FILE="${REPO}/filtered_arxiv.rss"
+ARXIV_RSS_BASE_URL="https://rss.arxiv.org/rss"
+RSS_DOWNLOAD_FILE="${REPO_PATH}/raw_arxiv.rss"
+RSS_OUTPUT_FILE="${REPO_PATH}/filtered_arxiv.rss"
 
 # for example filter quantum machine learning, use quant-ph as the main branch
-LINK_SUFFIX="quant-ph"
+RSS_CATEGORY="quant-ph"
 # Define the categories to filter
 CATEGORIES=("cs.AI" "cs.LG" "stat.ML" "cs.CC" "cs.IT")
 # Construct the category list
@@ -18,20 +18,19 @@ done
 CATEGORY_FILTER="${CATEGORY_FILTER:4}"  # Remove the leading ' or '
 
 # download the rss file
-curl -s "$BASE_URL/$LINK_SUFFIX" -o $DOWNLOAD_FILE
+curl -s "$ARXIV_RSS_BASE_URL/$RSS_CATEGORY" -o $RSS_DOWNLOAD_FILE
 # delete the replace paper
-xmlstarlet ed -d "//item[count(category)!=1 and not($CATEGORY_FILTER) or (arxiv:announce_type='replace') or (arxiv:announce_type='replace-cross')]" $DOWNLOAD_FILE > $OUTPUT_FILE
+xmlstarlet ed -d "//item[count(category)!=1 and not($CATEGORY_FILTER) or (arxiv:announce_type='replace') or (arxiv:announce_type='replace-cross')]" $RSS_DOWNLOAD_FILE > $RSS_OUTPUT_FILE
 
 echo "Today's date: $(date)"
-ORIGIN_ITEM_COUNT=$(xmlstarlet sel -t -v "count(//item)" $DOWNLOAD_FILE)
-ITEM_COUNT=$(xmlstarlet sel -t -v "count(//item)" $OUTPUT_FILE)
+ORIGIN_ITEM_COUNT=$(xmlstarlet sel -t -v "count(//item)" $RSS_DOWNLOAD_FILE)
+ITEM_COUNT=$(xmlstarlet sel -t -v "count(//item)" $RSS_OUTPUT_FILE)
 echo "Number of total items: $ORIGIN_ITEM_COUNT"
 echo "Number of items left: $ITEM_COUNT"
 
-#
-cd ${REPO}
+
+cd ${REPO_PATH}
 # comit add push to github page
 git add --all
 git commit -m "Update $(date)"
 git push origin
-
